cd99d1e8fa12a7eedb7598ebc48cb69cd212b340
Simon Genin <simongenin@hotmail.fr>
2017-02-12 17:01:27 +0100

    [visitor] start of the visitor class

--- src/main/java/be/unamur/info/b314/compiler/main/ASTVisitor.java
+++ src/main/java/be/unamur/info/b314/compiler/main/ASTVisitor.java
@@ -1,6 +1,9 @@
 package be.unamur.info.b314.compiler.main;
 
 import be.unamur.info.b314.compiler.B314BaseVisitor;
+import be.unamur.info.b314.compiler.B314Parser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class is the visitor of our tree.
@@ -11,6 +14,28 @@ import be.unamur.info.b314.compiler.B314BaseVisitor;
 public class ASTVisitor extends B314BaseVisitor
 {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ASTVisitor.class);
 
+    /**
+     * Steps :
+     *      Check preconditions
+     *      Check type matching
+     *      Update SymTable
+     *      determine the semantic
+     *      Generate PCode
+     */
+
+    @Override
+    public Object visitRoot (B314Parser.RootContext ctx)
+    {
+        LOG.debug("Visitor starts");
+
+        // We go the the children
+        super.visitRoot(ctx);
+
+        LOG.debug("Visitor ends");
+
+        return null;
+    }
 
 }
